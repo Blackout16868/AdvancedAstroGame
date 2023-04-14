8526e47f9acc0aa0f871fd0bf8e2fe2a7207ea51
Sewaddle36 <44526754+Sewaddle36@users.noreply.github.com>
2023-04-14 12:35:36 -0500

    Pause Menu
    Added a pause menu

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GamePaused = false;
+    
+    public GameObject pauseMenuUI;
+    void Start()
+    {
+        Resume();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GamePaused)
+            {
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+
+        CheckCursorLocked();
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+    }
+
+    void CheckCursorLocked()
+        {
+            if (GamePaused)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+}
