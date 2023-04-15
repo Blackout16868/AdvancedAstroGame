157094ef6c9c3980ea45b3032356329f7ec66e5b
Blackout16868 <Brady.ratzmann@gmail.com>
2023-04-15 08:44:07 -0500

    Added the ability for the user to see when their gun is overheating

--- Assets/Scripts/HeatBar.cs
+++ Assets/Scripts/HeatBar.cs
@@ -9,6 +9,7 @@ public class HeatBar : MonoBehaviour
     public float current;
     public Image mask;
     public GameObject Weapon;
+    public Image background;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +27,13 @@ public class HeatBar : MonoBehaviour
 
     void GetCurrentFill()
     {
+        if (!Weapon.GetComponent<Weapon>().getRun())
+        {
+         background.color =  new Color(1f, 0f, 0f);
+        }else 
+        {
+            background.color =  new Color(0.21f, 0.21f, 0.21f);
+        }
         float fillAmount = current/maximum;
         mask.fillAmount = fillAmount;
     }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -98,5 +98,10 @@ public class Weapon : MonoBehaviour
           }
         }
     }
+
+    public bool getRun()
+    {
+      return run;
+    }
     
   }
